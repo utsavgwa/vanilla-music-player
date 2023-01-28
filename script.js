76d10f9fec36d81ff369001403593e44a4ce595c
Utsav Singh <utsavgwa@gmail.com>
2023-01-29 04:44:56 +0530

    song array populated

--- script.js
+++ script.js
@@ -1,8 +1,45 @@
+const image = document.querySelector("img");
+const title = document.getElementById("title");
+const artist = document.getElementById("artist");
+
 const music = document.querySelector("audio");
 const prevBtn = document.getElementById("prev");
 const playBtn = document.getElementById("play");
 const nextBtn = document.getElementById("next");
 
+// array of songs to insert
+const songs = [
+  {
+    name: "audio1",
+    displayName: "Audio Clip One",
+    artist: "Artist One",
+  },
+  {
+    name: "audio2",
+    displayName: "Audio Clip Two",
+    artist: "Artist Two",
+  },
+  {
+    name: "audio3",
+    displayName: "Audio Clip Three",
+    artist: "Artist Three",
+  },
+  {
+    name: "audio4",
+    displayName: "Audio Clip Four",
+    artist: "Artist Four",
+  },
+  {
+    name: "audio5",
+    displayName: "Audio Clip Five",
+    artist: "Artist Five",
+  },
+  {
+    name: "audio6",
+    displayName: "Audio Clip Six",
+    artist: "Artist Six",
+  },
+];
 // boolean to check play or pause
 let isPlaying = false;
 
@@ -24,3 +61,14 @@ function pauseSong() {
 
 // play or pause button event listener
 playBtn.addEventListener("click", () => (isPlaying ? pauseSong() : playSong()));
+
+// function that add songs in DOM elements
+function loadSong(song) {
+  title.textContent = song.displayName;
+  artist.textContent = song.artist;
+  music.src = `./music/${song.name}.mp3`;
+  image.src = "https://source.unsplash.com/random/300×400/?song";
+}
+
+// on loading - selecting first song
+loadSong(songs[4]);
