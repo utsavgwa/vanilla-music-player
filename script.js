26560d1eff11b770dc77dde70dbcea75fde057b2
Utsav Singh <utsavgwa@gmail.com>
2023-01-29 07:49:13 +0530

    auto next song function

--- script.js
+++ script.js
@@ -144,3 +144,5 @@ nextBtn.addEventListener("click", nextSong);
 
 music.addEventListener("timeupdate", updateProgressBar);
 progressContainer.addEventListener("click", setProgressBar);
+
+music.addEventListener("ended", nextSong);
